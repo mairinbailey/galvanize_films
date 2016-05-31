19a0a91da3182ef3f2f026fb300fcc60e9c086a2
Mairin Bailey <mairin.bailey@gmail.com>
2016-05-31 16:31:22 -0600

    add landing page and form

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+var postEndpoint=
+
 $(document).ready(function(){
-  console.log("sanity check");
+
+var $title = $("#title").val();
+var $genre = $("#genre").val();
+var $description = $("#description").val();
+var $url = $("url").val();
+var $rating = $("#rating").val();
+
+$("#search").click(function(event){
+  event.preventDefault();
 });
+});
+
+// return {
+// title: form.find("#title").val();
+// genre: form.find("#genre").val();
+// description: form.find("#description").val()
+//
+// }
+
+//
